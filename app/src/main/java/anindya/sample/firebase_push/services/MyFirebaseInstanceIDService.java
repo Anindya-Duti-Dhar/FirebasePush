bf3874ed743a3101002338cd5fdd4466c4336317
Duti <anindyadutidhar@yahoo.com>
2017-11-09 12:24:57 +0600

    first commit

---
+++ app/src/main/java/anindya/sample/firebase_push/services/MyFirebaseInstanceIDService.java
@@ -0,0 +1,30 @@
+package anindya.sample.firebase_push.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "FirebaseID-duti";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        //calling the method store token and passing token
+        storeToken(refreshedToken);
+    }
+
+    private void storeToken(String token) {
+        //we will save the token in sharedpreferences later
+
+    }
+}
